21b5921d71b78ff2691f4211fac746d0bf7b9f06
Abhay Cherian Kuruvilla <151843354+abhay123123@users.noreply.github.com>
2024-06-28 21:25:43 +0530

    Add files via upload

---
+++ src/tailwind.jsx
@@ -0,0 +1,78 @@
+export default function App() {
+  return (<>
+    <h1 className="w-full text-center text-lg text-green-500">
+      Hello world!
+    </h1>
+   <div className=" border-2 w-full h-10 bg-violet-500 border-violet-600">
+    <p className="text-center  font-mono font-extralight">A div</p>
+    
+  
+
+
+    
+    </div> 
+    <br />
+    <div className=" fixed w-10 h-10 bg-red-400 top-0"></div>
+      <div className="flex justify-center space-x-7">
+      <div className="w-10 h-10 rounded-full bg-blue-300"></div>
+      <div className="w-10 h-10 rounded-full bg-blue-300"></div>
+      <div></div>
+
+      </div>
+
+
+    <div className="grid  grid-cols-3 gap-5">
+      <div className=" bg-violet-400 h-12"></div>
+      <div className=" bg-violet-400 h-12"></div>
+      
+      <div className=" bg-violet-400 h-12"></div>
+
+
+      
+
+
+
+      
+
+
+
+
+      
+      
+      
+      </div>
+      <button className="my-2 rounded-lg bg-blue-800 px-4 py-2  text-white hover:bg-blue-300"> CLICK ME</button> 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      </>
+
+
+
+  
+  )
+}
